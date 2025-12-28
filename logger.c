deae7b407829350b48e08f715128bf9c5922b281
kioz.wang <never.had@outlook.com>
2025-12-29 02:12:18 +0800

    Feat: use independent instances instead of global variables

--- logger.c
+++ logger.c
@@ -2,7 +2,7 @@
  * @file logger.c
  * @author kioz.wang
  * @brief
- * @version 0.2
+ * @version 1.0
  * @date 2025-10-09
  *
  * @copyright MIT License
@@ -29,29 +29,21 @@
  */
 
 #include "logger.h"
+#include <assert.h>
 #include <limits.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#ifndef MLOGGER_FMT_MAX
-#define MLOGGER_FMT_MAX (1024)
-#endif /* MLOGGER_FMT_MAX */
-
-static mlog_level_t g_log_level = MLOG_DEBG;
-
-static void default_logger(const char *msg) { fputs(msg, stdout); }
-
-static mlogger_f g_logger = default_logger;
-
-static const char *log_level_names[MLOG_DEBG + 1] = {
+static const char *level_names[MLOG_DEBG + 1] = {
     "ERRO", "WARN", "INFO", "VERB", "DEBG",
 };
 
 mlog_level_t mlog_level_parse(const char *s) {
     for (int32_t i = 0; i < MLOG_DEBG + 1; i++) {
-        if (!strcmp(s, log_level_names[i])) {
+        if (!strcmp(s, level_names[i])) {
             return i;
         }
     }
@@ -69,64 +61,117 @@ mlog_level_t mlog_level_parse(const char *s) {
     return value;
 }
 
-static int32_t check_stderr_level() {
-    static int32_t value   = -2;
-    const char    *value_s = NULL;
-
-    if (value == -2) {
-#ifdef MLOGGER_ENV
-        value_s = getenv(MLOGGER_ENV);
-#endif /* MLOGGER_ENV */
-        if (!value_s || value_s[0] == '\0') {
-            value = -1;
+static bool mlog_level_parse_env(const char *envname, mlog_level_t *level) {
+    if (envname) {
+        const char *level_s = getenv(envname);
+        if (level_s && level_s[0]) {
+            *level = mlog_level_parse(level_s);
+            return true;
         }
     }
-    if (value != -2) return value;
-
-    return value = mlog_level_parse(value_s);
+    return false;
 }
 
-static void logf__(mlog_level_t lvl, const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
+void mlog_init(mlogger_t *mlogger, const char *envname, const char *envname_stderr, uint8_t fmt_cfg,
+               uint8_t fmt_cfg_stderr) {
+    mlogger->level = MLOG_ERRO;
+    mlogger->f     = NULL;
+    mlog_level_parse_env(envname, &mlogger->level);
+    mlogger->enable_stderr  = mlog_level_parse_env(envname_stderr, &mlogger->level_stderr);
+    mlogger->fmt_cfg        = fmt_cfg;
+    mlogger->fmt_cfg_stderr = fmt_cfg_stderr;
+}
 
-    int32_t stderr_level = check_stderr_level();
+void mlog_set_logger(mlogger_t *mlogger, mlog_level_t level, mlog_f f) {
+    mlogger->level = level > MLOG_DEBG ? MLOG_DEBG : level;
+    if (f) mlogger->f = f;
+}
 
-    if ((stderr_level >= 0 && lvl <= stderr_level) || lvl <= g_log_level) {
-        char line[MLOGGER_FMT_MAX] = {0};
-        (void)vsnprintf(line, sizeof(line), fmt, args);
+void mlog_enable_stderr(mlogger_t *mlogger, mlog_level_t level) {
+    mlogger->level_stderr  = level > MLOG_DEBG ? MLOG_DEBG : level;
+    mlogger->enable_stderr = true;
+}
 
-        if (stderr_level >= 0 && lvl <= stderr_level) fputs(line, stderr);
-        if (lvl <= g_log_level) g_logger(line);
+#ifndef MLOGGER_FMT_MAX
+#define MLOGGER_FMT_MAX (1024)
+#endif /* MLOGGER_FMT_MAX */
+
+#if MLOGGER_FMT_MAX < 32
+#error MLOGGER_FMT_MAX too small
+#endif
+
+static const char *colors[MLOG_DEBG + 1] = {
+    "\e[31m",               /* 红 */
+    "\e[33m",               /* 黄 */
+    "\e[32m",               /* 绿 */
+    "\e[38;2;100;150;100m", /* 浅绿 */
+    "\e[90m",               /* 灰 */
+};
+#define color_reset "\e[0m"
+#define color_bold  "\e[1m"
+
+static const char *level_heads[MLOG_DEBG + 1] = {
+    "[E]", "[W]", "[I]", "[V]", "[D]",
+};
+
+static int64_t timestamp(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1000000L + ts.tv_nsec / 1000L;
+}
+
+static void mlog_format(uint8_t cfg, mlog_level_t level, char *restrict __s, size_t __maxlen,
+                        const char *restrict __format, __gnuc_va_list __arg) {
+    int pos = 0;
+
+    if (cfg & MLOG_FMT_NEWLINE) __maxlen--;
+    if (cfg & MLOG_FMT_COLOR) __maxlen -= sizeof(color_reset) - 1;
+
+    if (cfg & MLOG_FMT_TIMESTAMP) pos += snprintf(&__s[pos], __maxlen - pos, "[%lx]", timestamp());
+    if (cfg & MLOG_FMT_COLOR) pos += snprintf(&__s[pos], __maxlen - pos, "%s", colors[level]);
+    if (cfg & MLOG_FMT_LEVEL_HEAD) pos += snprintf(&__s[pos], __maxlen - pos, "%s", level_heads[level]);
+    if (cfg & MLOG_FMT_COLOR && level == MLOG_ERRO) pos += snprintf(&__s[pos], __maxlen - pos, "%s", color_bold);
+
+    assert(__maxlen - pos);
+    int n = vsnprintf(&__s[pos], __maxlen - pos, __format, __arg);
+    if (n < __maxlen - pos) pos += n;
+    else pos = __maxlen;
+
+    if (cfg & MLOG_FMT_COLOR) pos += sprintf(&__s[pos], "%s", color_reset);
+    if (cfg & MLOG_FMT_NEWLINE) sprintf(&__s[pos], "%c", '\n');
+}
+
+void mlogf(mlogger_t *mlogger, mlog_level_t level, const char *fmt, ...) {
+    char line[MLOGGER_FMT_MAX] = {0};
+
+    if ((mlogger->enable_stderr && level <= mlogger->level_stderr)) {
+        va_list args;
+        va_start(args, fmt);
+        mlog_format(mlogger->fmt_cfg_stderr, level, line, sizeof(line), fmt, args);
+        va_end(args);
+        fputs(line, stderr);
     }
-
-    va_end(args);
+    if (mlogger->f && level <= mlogger->level) {
+        va_list args;
+        va_start(args, fmt);
+        mlog_format(mlogger->fmt_cfg, level, line, sizeof(line), fmt, args);
+        va_end(args);
+        mlogger->f(line);
+    }
 }
-
-static void set_logger(mlog_level_t lvl, mlogger_f f) {
-    g_log_level = (lvl > MLOG_DEBG) ? MLOG_DEBG : lvl;
-    if (f) g_logger = f;
-}
-
-static void set_out_logger(void (*f)(mlog_level_t, mlogger_f)) { f(g_log_level, g_logger); }
-
-void MLOGGER_FUNC(logf)(mlog_level_t lvl, const char *fmt, ...) __attribute__((alias("logf__")));
-void MLOGGER_FUNC(set_logger)(mlog_level_t lvl, mlogger_f f) __attribute__((alias("set_logger")));
-void MLOGGER_FUNC(set_out_logger)(void (*f)(mlog_level_t, mlogger_f)) __attribute__((alias("set_out_logger")));
 
 #ifdef __TEST_LOGGER__
 
 int32_t main(int32_t argc, const char *argv[]) {
-    set_logger(MLOG_INFO, NULL);
+    mlogger_t mlogger;
+
+    mlog_init(&mlogger, NULL, "log2stderr", MLOG_FMT_LEVEL_HEAD,
+              MLOG_FMT_COLOR | MLOG_FMT_TIMESTAMP | MLOG_FMT_LEVEL_HEAD | MLOG_FMT_NEWLINE);
+    mlog_set_logger(&mlogger, MLOG_INFO, (mlog_f)puts);
+
     for (int32_t i = 0; i < MLOG_DEBG + 1; i++) {
-        logf__(i, "message level %s\n", log_level_names[i]);
+        mlogf(&mlogger, i, "hello %s", "world");
     }
-    logf__(MLOG_ERRO, "\n");
-    logfE("%s", log_level_names[MLOG_ERRO]);
-    logfW("%s", log_level_names[MLOG_WARN]);
-    logfI("%s", log_level_names[MLOG_INFO]);
-    logfV("%s", log_level_names[MLOG_VERB]);
-    logfD("%s", log_level_names[MLOG_DEBG]);
 }
 
 #endif /* __TEST_LOGGER__ */
--- logger.h
+++ logger.h
@@ -2,7 +2,7 @@
  * @file logger.h
  * @author kioz.wang
  * @brief
- * @version 0.2
+ * @version 1.0
  * @date 2025-10-09
  *
  * @copyright MIT License
@@ -31,6 +31,7 @@
 #ifndef __LOGGER_H__
 #define __LOGGER_H__
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -48,79 +49,74 @@ enum mlog_level {
 };
 typedef uint8_t mlog_level_t;
 
+/**
+ * @brief 解析字符串到 mlog_level_t，一般用在命令行解析函数中
+ *
+ * @param s "ERRO", "WARN", "INFO", "VERB", "DEBG"，或数字（不限进制）
+ * @return mlog_level_t 始终返回 mlog_level_t 范围内的值（无法解析或越界的将降为 MLOG_DEBG）
+ */
 mlog_level_t mlog_level_parse(const char *s);
 
-typedef void (*mlogger_f)(const char *msg);
+typedef void (*mlog_f)(const char *msg);
 
-#define MLOGGER_SYMBOL_PREFIX _
+/* 格式化配置：使能颜色，按等级配置在 `colors[]` */
+#define MLOG_FMT_COLOR (1u << 0)
+/* 格式化配置：行首输出时间戳，格式`[%lx]`，单位 ns */
+#define MLOG_FMT_TIMESTAMP (1u << 1)
+/* 格式化配置：行首输出等级标识，按等级配置在 `level_heads[]` */
+#define MLOG_FMT_LEVEL_HEAD (1u << 2)
+/* 格式化配置：行尾换行 */
+#define MLOG_FMT_NEWLINE (1u << 3)
 
-#define __SYMBOL_CONCAT__(a, b)        a##b
-#define __MLOGGER_FUNC__(prefix, name) __SYMBOL_CONCAT__(prefix, name)
-#define MLOGGER_FUNC(name)             __MLOGGER_FUNC__(MLOGGER_SYMBOL_PREFIX, name)
+struct mlogger {
+    mlog_level_t level;
+    mlog_f       f;
+    mlog_level_t level_stderr;
+    bool         enable_stderr;
+    uint8_t      fmt_cfg;
+    uint8_t      fmt_cfg_stderr;
+};
+typedef struct mlogger mlogger_t;
 
-void MLOGGER_FUNC(logf)(mlog_level_t lvl, const char *fmt, ...) __attribute__((format(printf, 2, 3)));
-void MLOGGER_FUNC(set_logger)(mlog_level_t lvl, mlogger_f f);
-void MLOGGER_FUNC(set_out_logger)(void (*f)(mlog_level_t, mlogger_f));
-
-#ifdef MLOGGER_COLOR
-#define MLOG_cRESET "\e[0m"
-#define MLOG_cBOLD  "\e[1m"
-#define MLOG_cERRO  "\e[31m"               /* 红 */
-#define MLOG_cWARN  "\e[33m"               /* 黄 */
-#define MLOG_cINFO  "\e[32m"               /* 绿 */
-#define MLOG_cVERB  "\e[38;2;100;150;100m" /* 浅绿 */
-#define MLOG_cDEBG  "\e[90m"               /* 灰 */
-#else
-#define MLOG_cRESET
-#define MLOG_cBOLD
-#define MLOG_cERRO
-#define MLOG_cWARN
-#define MLOG_cINFO
-#define MLOG_cVERB
-#define MLOG_cDEBG
-#endif /* MLOGGER_COLOR */
-
-#ifdef MLOGGER_TIMESTAMP
-#include <stdbool.h>
-#include <time.h>
-static inline int64_t mlog_timestamp(bool monotonic) {
-    struct timespec ts;
-    clock_gettime(monotonic ? CLOCK_MONOTONIC : CLOCK_REALTIME, &ts);
-    return ts.tv_sec * 1000000000L + ts.tv_nsec;
-}
-#define MLOG_tsFmt "[%lx]"
-#define MLOG_tsArg , (mlog_timestamp(true) / 1000)
-#else
-#define MLOG_tsFmt
-#define MLOG_tsArg
-#endif /* MLOGGER_TIMESTAMP */
-
-#ifdef MLOGGER_LEVEL
-#define MLOG_lERRO "[E]"
-#define MLOG_lWARN "[W]"
-#define MLOG_lINFO "[I]"
-#define MLOG_lVERB "[V]"
-#define MLOG_lDEBG "[D]"
-#else
-#define MLOG_lERRO
-#define MLOG_lWARN
-#define MLOG_lINFO
-#define MLOG_lVERB
-#define MLOG_lDEBG
-#endif /* MLOGGER_LEVEL */
-
-#define logfE(fmt, ...)                                                                                                \
-    MLOGGER_FUNC(logf)(MLOG_ERRO, MLOG_tsFmt MLOG_cERRO MLOG_lERRO MLOG_cBOLD fmt MLOG_cRESET "\n" MLOG_tsArg,         \
-                       ##__VA_ARGS__)
-#define logfW(fmt, ...)                                                                                                \
-    MLOGGER_FUNC(logf)(MLOG_WARN, MLOG_tsFmt MLOG_cWARN MLOG_lWARN MLOG_cBOLD fmt MLOG_cRESET "\n" MLOG_tsArg,         \
-                       ##__VA_ARGS__)
-#define logfI(fmt, ...)                                                                                                \
-    MLOGGER_FUNC(logf)(MLOG_INFO, MLOG_tsFmt MLOG_cINFO MLOG_lINFO fmt MLOG_cRESET "\n" MLOG_tsArg, ##__VA_ARGS__)
-#define logfV(fmt, ...)                                                                                                \
-    MLOGGER_FUNC(logf)(MLOG_VERB, MLOG_tsFmt MLOG_cVERB MLOG_lVERB fmt MLOG_cRESET "\n" MLOG_tsArg, ##__VA_ARGS__)
-#define logfD(fmt, ...)                                                                                                \
-    MLOGGER_FUNC(logf)(MLOG_DEBG, MLOG_tsFmt MLOG_cDEBG MLOG_lDEBG fmt MLOG_cRESET "\n" MLOG_tsArg, ##__VA_ARGS__)
+/**
+ * @brief 初始化 mlogger
+ * @note 日志接口初始化为 NULL，此时实际不记录日志。需要通过 `mlog_set_logger` 设置。
+ *
+ * @param mlogger
+ * @param envname 根据环境变量初始化日志等级（默认 MLOG_ERRO）
+ * @param envname_stderr 根据环境变量使能 stderr 输出，并初始化日志等级（默认不输出）
+ * @param fmt_cfg 格式化配置（`MLOG_FMT_` 按位或）
+ * @param fmt_cfg_stderr stderr 输出的格式化配置（`MLOG_FMT_` 按位或）
+ */
+void mlog_init(mlogger_t *mlogger, const char *envname, const char *envname_stderr, uint8_t fmt_cfg,
+               uint8_t fmt_cfg_stderr);
+/**
+ * @brief 设置日志等级和日志接口
+ *
+ * @param mlogger
+ * @param level 越界的将被降为 MLOG_DEBG
+ * @param f （传入 NULL 时维持原接口）
+ */
+void mlog_set_logger(mlogger_t *mlogger, mlog_level_t level, mlog_f f);
+/**
+ * @brief 使能 stderr 输出，并设置日志等级
+ *
+ * @param mlogger
+ * @param level 越界的将被降为 MLOG_DEBG
+ */
+void mlog_enable_stderr(mlogger_t *mlogger, mlog_level_t level);
+/**
+ * @brief 记录日志
+ * @note
+ * 日志格式化后通过接口输出，格式化可能会截断日志，但当使能了颜色或换行时，颜色始终会在尾部重置，换行始终有效。格式化栈内存为
+ * MLOGGER_FMT_MAX（默认 1024），可在编译时通过 `-DMLOGGER_FMT_MAX=?` 覆盖
+ *
+ * @param mlogger
+ * @param level
+ * @param fmt
+ * @param ...
+ */
+void mlogf(mlogger_t *mlogger, mlog_level_t level, const char *fmt, ...);
 
 #ifdef __cplusplus
 }
